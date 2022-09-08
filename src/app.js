9047a086572a3ddfd7841fae0ecc09889fb03651
Fernando Orozco <forozcomercado@gmail.com>
2022-09-08 15:09:15 -0500

    ADD cors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 const clientRoutes = require("./clients/client.routes");
 
 app.set("port", process.env.PORT);
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/clients", clientRoutes);
