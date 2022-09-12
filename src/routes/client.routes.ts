52b47eb330ca35ac0beec5846fd3e909f2a89a50
Fernando Orozco <forozcomercado@gmail.com>
2022-09-12 07:06:24 -0500

    Add user login interface REMOVE search client routes and controller

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 const router = Router();
-const clientController = require("../controllers/clients.controller");
+import clientController from "../controllers/clients.controller";
 
 router.get("/", clientController.getClients);
-router.get("/:name", clientController.searchClients);
-
 router.post("/", clientController.registerNewClient);
 router.delete("/:id", clientController.removeClient);
-export default router;
+export const clientRoutes = router;
